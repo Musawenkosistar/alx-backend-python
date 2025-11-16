25df5c37146599f4742e00e36707e08e8ad6d8cb
Musawenkosistar <musawenkosi2504@gmail.com>
2025-11-16 08:57:12 +0000

    messaging_app

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# messaging_app/urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('chats.urls')),              # Include chat app API
+    path('api-auth/', include('rest_framework.urls')),  # DRF login/logout
 ]
